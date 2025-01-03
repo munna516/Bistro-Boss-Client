44bbf0583dfa4883def92d66262164ae48bb14a7
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2025-01-03 12:13:18 +0600

    card delete done

--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -1,9 +1,36 @@
+import Swal from "sweetalert2";
 import useCart from "../../../Hooks/useCart";
 import { MdDelete } from "react-icons/md";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const Cart = () => {
-  const [cart] = useCart();
+  const [cart, refetch] = useCart();
   const totalPrice = cart.reduce((prev, item) => prev + item.price, 0);
+  const axiosSecure = useAxiosSecure();
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/carts/${id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
   return (
     <div>
       <div className="flex items-center justify-evenly">
@@ -43,7 +70,10 @@ const Cart = () => {
                 <td>{item.name}</td>
                 <td>{item.price}</td>
                 <th>
-                  <button className="btn btn-ghost btn-xs">
+                  <button
+                    onClick={() => handleDelete(item._id)}
+                    className="btn btn-ghost btn-xs"
+                  >
                     <MdDelete className="text-xl text-red-600"></MdDelete>
                   </button>
                 </th>
